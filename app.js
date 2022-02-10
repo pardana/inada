98267bd4e728117ec2dfd96ae7278efe85965974
Aditria Pardana <aditria.pardana20@gmail.com>
2022-02-10 11:34:43 +0700

    Add event listener

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-console.log("Music");
-const audio = document.querySelector('audio');
-const playButton = document.querySelector('#play-button');
+console.log("Music")
+const audio = document.querySelector('audio')
+const playButton = document.querySelector('#play-button')
+let isSongPlaying = false
+
+audio.onplaying = event => {
+    console.log('onplaying')
+    isSongPlaying = true
+}
 
 playButton.addEventListener('click', () => {
-    console.log('playButton clicked');
+    console.log('playButton clicked')
+    audio.play()
 })
