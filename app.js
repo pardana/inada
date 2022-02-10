63b1d72673ec98d820fa2c88053eecc7171b19f2
Aditria Pardana <aditria.pardana20@gmail.com>
2022-02-10 13:56:38 +0700

    Add progressBar and duration

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const audio = document.querySelector('audio')
 const playButton = document.querySelector('#play-button')
+const progressBar = document.getElementById('progress')
+const durasiLagu = document.querySelector('.durasi')
 let isSongPlaying = false
 
 audio.onplaying = event => {
@@ -12,6 +14,17 @@ audio.onpause = event => {
     console.log('onpause')
     isSongPlaying = false
     playButton.innerHTML = 'Play'
+}
+
+audio.ontimeupdate = event => {
+    console.log('ontimeupdate')
+    const { currentTime, duration } = event.srcElement;
+    const percent = (currentTime / duration) * 100
+    const minutes = Math.floor(duration/60)
+    const seconds = Math.floor(duration- (minutes*60))
+    console.log('percent', percent)
+    progressBar.style.width = `${percent}%`
+    durasiLagu.innerHTML = `${minutes}:${seconds}`
 }
 
 playButton.addEventListener('click', () => {
