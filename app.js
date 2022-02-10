85e9a0dd7d7c421b44272555b35e50f093aed924
Aditria Pardana <aditria.pardana20@gmail.com>
2022-02-10 11:26:17 +0700

    Add app.js and app.css

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Music");
+const audio = document.querySelector('audio');
+const playButton = document.querySelector('#play-button');
+
+playButton.addEventListener('click', () => {
+    console.log('playButton clicked');
+})
