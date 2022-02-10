30af42300cf183a797d47fd7265ec76daa316bd9
Aditria Pardana <aditria.pardana20@gmail.com>
2022-02-10 13:38:36 +0700

    Add event onplaying and onpause

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ let isSongPlaying = false
 audio.onplaying = event => {
     console.log('onplaying')
     isSongPlaying = true
+    playButton.innerHTML = 'Pause'
+}
+
+audio.onpause = event => {
+    console.log('onpause')
+    isSongPlaying = false
+    playButton.innerHTML = 'Play'
 }
 
 playButton.addEventListener('click', () => {
     console.log('playButton clicked')
+    if(isSongPlaying){
+        audio.pause()
+        return
+    }
+     
     audio.play()
 })
